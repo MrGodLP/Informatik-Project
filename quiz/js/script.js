21809ff0ca9de866be478d874c9b17bdcafc0808
MrGodLP <78224892+MrGodLP@users.noreply.github.com>
2022-04-18 23:37:03 +0200

    t

--- quiz/js/script.js
+++ quiz/js/script.js
@@ -1,6 +1,5 @@
 //selecting all required elements
 const start_btn = document.querySelector(".start_btn button");
-const leaf_btn = document.querySelector(".leaf_btn button")
 const info_box = document.querySelector(".info_box");
 const exit_btn = info_box.querySelector(".buttons .quit");
 const continue_btn = info_box.querySelector(".buttons .restart");
